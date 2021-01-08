3cb9324f96b7b5934c9a524d331ffb023c46a575
1b3r1c0 <baconandeggs@pm.me>
2021-01-07 22:13:15 -0500

    sample completed

--- urllink2.py
+++ urllink2.py
@@ -1,42 +1,31 @@
-#!/bin/python
+#!/bin/python3
 
-# To run this, download the BeautifulSoup zip file
-# http://www.py4e.com/code3/bs4.zip
-# and unzip it in the same directory as this file
+from bs4 import BeautifulSoup # Install bs4: pip3 install bs4
+import urllib
+import ssl
+ 
+# Use the "ssl" library to create a "contex"
+# The ssl contex will be used in urllib's urlopen method
+# The purpose of this context is to ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
-# from urllib.request import urlopen
-# import urllib
-#.request
-from bs4 import BeautifulSoup
-# import ssl
-# 
-# # Use the "ssl" library to create a "contex"
-# # The ssl contex will be used in urllib's urlopen method
-# # The purpose of this context is to ignore SSL certificate errors
-# ctx = ssl.create_default_context()
-# ctx.check_hostname = False
-# ctx.verify_mode = ssl.CERT_NONE
-# 
-# # url = input('Enter - ')
-# # The sum for the "Sample Data" page is 2553
-# url = "http://py4e-data.dr-chuck.net/comments_42.html"
-# 
-# # old way from py4e's script
-# # html = urlopen(url, context=ctx).read()
-# 
-# # New way
-# with urllib.request.urlopen(url, context=ctx) as response:
-#    html = response.read()
-# 
-# soup = BeautifulSoup(html, "html.parser")
-# 
-# # Retrieve all of the anchor tags
-# tags = soup('a')
-# for tag in tags:
-#     # Look at the parts of a tag
-#     print('TAG:', tag)
-#     # debug
-#     junk = input("CTL+c to quit, RTN to continue")
-#     print('URL:', tag.get('href', None))
-#     print('Contents:', tag.contents[0])
-#     print('Attrs:', tag.attrs)
+print("The sum for this Sample Data page is 2553")
+
+url = "http://py4e-data.dr-chuck.net/comments_42.html"
+
+with urllib.request.urlopen(url, context=ctx) as response:
+   html = response.read()
+
+soup = BeautifulSoup(html, "html.parser")
+
+print("this scripts sum:", sum( int(x.text) for x in soup.find_all('span') ) )
+
+# Pull out all "span" tags
+# soup.find_all('span')
+
+# Use the "text" method to get the number (as a string) out of a span tag
+# x.text
+
+## debug for each print(tags)junk = input("CTL+c to quit, RTN to continue")
